f27bc00aac8db0c65cb7241f01986ad4343f75ba
Anurag2575 <anuragdeshmukh2575@gmail.com>
2025-03-22 02:49:48 +0530

    1. Two Sum

---
+++ Arrays/1_twoSum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target){
+    unordered_map<int, int> mpp;
+    for(int i = 0; i < nums.size(); i++){
+        int complement = target - nums[i];
+        if(mpp.find(complement) != mpp.end()){
+            return {mpp[complement], i};
+        }
+        mpp[nums[i]] = i;
+    }
+    return {-1, -1};
+}

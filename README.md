4eb05ceb40235ff8fc79db7cc1a8a5c19a1d2ea2
Anurag2575 <anuragdeshmukh2575@gmail.com>
2025-03-29 01:41:23 +0530

    Deleted Everything

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a repository for DSA Questions on LeetCode.
--- SQL/176_secondHighestSalary.sql
+++
@@ -1,4 +0,0 @@
-select (select max(salary)
-    from Employee
-    where salary < (select max(salary) from employee))
-as SecondHighestSalary;

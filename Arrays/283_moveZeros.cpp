7070858090287fea90b54ad2e07a2a44a9e96547
Anurag2575 <anuragdeshmukh2575@gmail.com>
2025-03-29 14:21:33 +0530

    283. Move Zeros

---
+++ Arrays/283_moveZeros.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void moveZeros(vector<int>& nums){
+    int j = 0;
+    for(int i = 0; i < nums.size(); i++)
+        if(nums[i]!=0) swap(nums[i], nums[j++]);
+}

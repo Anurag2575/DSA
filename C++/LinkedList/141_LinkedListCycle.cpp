6c3b0c5c8407be9a89c46dcd8b32a436e7ba7867
Anurag2575 <anuragdeshmukh2575@gmail.com>
2025-04-07 00:40:38 +0530

    141. Linked List Cycle

---
+++ C++/LinkedList/141_LinkedListCycle.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+bool hasCycle(ListNode* head){
+  ListNode* slow = head, fast = head;//tortoise and hare algo.
+
+  while(fast && fast->next){
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if(slow == fast) return true;//cycle detected
+  }
+
+  return false;
+}
